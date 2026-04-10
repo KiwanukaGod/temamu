7582508c61d9f7432c3125252d73f1d84b0e1641
Kiwanuka Godfrey <kiwanukagodfrey21@gmail.com>
2026-04-10 13:05:02 +0300

    Refoctor: Change the "add friend text" on the button to "continue".

--- lib/features/auth/screens/create_pool_screen.dart
+++ lib/features/auth/screens/create_pool_screen.dart
@@ -104,7 +104,7 @@ class _CreatePoolScreenState extends State<CreatePoolScreen> {
                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
                     ),
                     child: const Text(
-                      "Next: Add Participants", 
+                      "Continue", 
                       style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)
                     ),
                   ),
